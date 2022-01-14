4d5dc12536764654df4c2ea378aa2aaf7a99708f
dreadwing5 <sachinspyk@outlook.com>
2022-01-14 20:31:00 +0530

    added password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { encrypt, compare } = require("../utils/passwordHelper");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password"],
+    validate: {
+      //This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password do not match!",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+  //Hash the password
+
+  this.password = await encrypt(this.password);
+
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined; //This is to prevent saving the passwordConfirm to the database
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -40,5 +40,3 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
-
-console.log(x);
