7461e44e1c3fc8a7b5f92cce121a32686e7122b9
dreadwing5 <sachinspyk@outlook.com>
2022-01-15 15:20:18 +0530

    Authorization - user roles and permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,3 +92,18 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide'] , req.user.role = 'user'  => no access
+
+    //403 = Forbidden
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+
+    next();
+  };
+};
