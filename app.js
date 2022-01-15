a5987c746b2f0d5781283193780b4fcc665e3257
dreadwing5 <sachinspyk@outlook.com>
2022-01-16 05:28:53 +0530

    adding security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -10,22 +11,33 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 //1) GLOBAL MIDDLEWARE
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-//allow 100 requests per hour per IP
-
 const limiter = rateLimit({
   max: 100,
   windowMS: 60 * 60 * 1000,
+  //allow 100 requests per hour per IP
   message: "Too many requests from this IP, please try again in an hour!",
 });
 app.use("/api", limiter);
 
-app.use(express.json());
+//Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(`Requested at ${req.requestTime}`);
@@ -34,14 +46,15 @@ app.use((req, res, next) => {
 });
 
 //routes
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+//Handle non-existing routes
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+//Error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
