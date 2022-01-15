1e354eb1665e3cd16dbc11ee023663f5a6d97691
dreadwing5 <sachinspyk@outlook.com>
2022-01-15 18:34:15 +0530

    added auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,15 @@ const signToken = (id) => {
   });
 };
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true, //don't let browser js access this cookie, used to prevent xss attacks
+};
+
+if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -18,11 +27,16 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+
   const token = signToken(newUser._id);
+
+  res.cookie("jwt", token, cookieOptions);
+
+  //Remove the password from the response
+  newUser.password = undefined;
 
   res.status(201).json({
     status: "success",
-    token,
     data: {
       user: newUser,
     },
